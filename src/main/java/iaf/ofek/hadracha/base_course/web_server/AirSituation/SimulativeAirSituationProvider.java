2103037c4be81280e7e53ec141f6f4dfc3819b6c
Roee Fligman <fligman1@gmail.com>
2020-06-11 10:51:44 +0300

    Change EjectionPilotInfo

--- src/main/java/iaf/ofek/hadracha/base_course/web_server/AirSituation/SimulativeAirSituationProvider.java
+++ src/main/java/iaf/ofek/hadracha/base_course/web_server/AirSituation/SimulativeAirSituationProvider.java
@@ -12,136 +12,151 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-/**
- * A simulator that manages airplanes and randomly advances them in position over time
- */
+/** A simulator that manages airplanes and randomly advances them in position over time */
 @Service
 public class SimulativeAirSituationProvider implements AirSituationProvider {
 
-    private static final double CHANCE_FOR_NUMBER_CHANGE = 0.005;
-    private static final double CHANCE_FOR_AZIMUTH_CHANGE = 0.05;
-    private static int STEP_SIZE = 15;
-    private static int SIMULATION_INTERVAL_MILLIS = 100;
-    private double LAT_MIN = 29.000;
-    private double LAT_MAX = 36.000;
-    private double LON_MIN = 32.000;
-    private double LON_MAX = 46.500;
-    private static final double AZIMUTH_STEP = STEP_SIZE / (2000.0 / SIMULATION_INTERVAL_MILLIS);
+  private static final double CHANCE_FOR_NUMBER_CHANGE = 0.005;
+  private static final double CHANCE_FOR_AZIMUTH_CHANGE = 0.05;
+  private static int STEP_SIZE = 15;
+  private static int SIMULATION_INTERVAL_MILLIS = 100;
+  private double LAT_MIN = 29.000;
+  private double LAT_MAX = 36.000;
+  private double LON_MIN = 32.000;
+  private double LON_MAX = 46.500;
+  private static final double AZIMUTH_STEP = STEP_SIZE / (2000.0 / SIMULATION_INTERVAL_MILLIS);
 
+  // Scheduler to run advancement task repeatedly
+  private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+  // Object that acts as a lock for the updates
+  private final Object lock = new Object();
+  private final Random random = new Random();
+  private final RandomGenerators randomGenerators;
+  private final GeographicCalculations geographicCalculations;
 
-    // Scheduler to run advancement task repeatedly
-    private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-    // Object that acts as a lock for the updates
-    private final Object lock = new Object();
-    private final Random random = new Random();
-    private final RandomGenerators randomGenerators;
-    private final GeographicCalculations geographicCalculations;
+  // The current air situation
+  private List<Airplane> airplanes = new ArrayList<>();
+  private int lastId = 0;
 
-    // The current air situation
-    private List<Airplane> airplanes = new ArrayList<>();
-    private int lastId = 0;
+  public SimulativeAirSituationProvider(
+      RandomGenerators randomGenerators, GeographicCalculations geographicCalculations) {
+    this.randomGenerators = randomGenerators;
+    this.geographicCalculations = geographicCalculations;
 
+    for (int i = 0; i < 80; i++) {
+      foo();
+    }
 
-    public SimulativeAirSituationProvider(RandomGenerators randomGenerators, GeographicCalculations geographicCalculations) {
-        this.randomGenerators = randomGenerators;
-        this.geographicCalculations = geographicCalculations;
+    executor.scheduleAtFixedRate(
+        this::UpdateSituation, 0, SIMULATION_INTERVAL_MILLIS, TimeUnit.MILLISECONDS);
+  }
 
-        for (int i = 0; i < 80; i++) {
-            foo();
+  // all airplane kinds that can be used
+  private List<AirplaneKind> airplaneKinds = AirplaneKind.LeafKinds();
+
+  private void foo() {
+    AirplaneKind kind = airplaneKinds.get(random.nextInt(airplaneKinds.size()));
+    Airplane airplane = new Airplane(kind, lastId++);
+    airplane.coordinates =
+        new Coordinates(
+            randomGenerators.generateRandomDoubleInRange(LAT_MIN, LAT_MAX),
+            randomGenerators.generateRandomDoubleInRangeWithNormalDistribution(LON_MIN, LON_MAX));
+    airplane.setAzimuth(randomGenerators.generateRandomDoubleInRange(0, 360));
+    airplane.velocity =
+        randomGenerators.generateRandomDoubleInRange(40, 70)
+            * airplane.getAirplaneKind().getVelocityFactor();
+    airplanes.add(airplane);
+  }
+
+  private void UpdateSituation() {
+    try {
+      synchronized (lock) {
+        if (random.nextDouble() < CHANCE_FOR_NUMBER_CHANGE) { // chance to remove an airplane
+          int indexToRemove;
+          do {
+            indexToRemove = random.nextInt(airplanes.size());
+          } while (airplanes.get(indexToRemove).isAllocated()); // don't remove allocated airplane
+          airplanes.remove(indexToRemove);
         }
 
-        executor.scheduleAtFixedRate(this::UpdateSituation, 0, SIMULATION_INTERVAL_MILLIS, TimeUnit.MILLISECONDS);
-    }
+        airplanes.forEach(
+            airplane -> {
+              airplane.radialAcceleration = calculateNewRadialAcceleration(airplane);
 
-    // all airplane kinds that can be used
-    private List<AirplaneKind> airplaneKinds = AirplaneKind.LeafKinds();
+              airplane.setAzimuth(airplane.getAzimuth() + airplane.radialAcceleration);
+              airplane.coordinates.lat +=
+                  Math.sin(worldAzimuthToEuclidRadians(airplane.getAzimuth()))
+                      * airplane.velocity
+                      / 100000;
+              airplane.coordinates.lon +=
+                  Math.cos(worldAzimuthToEuclidRadians(airplane.getAzimuth()))
+                      * airplane.velocity
+                      / 100000;
+              if (airplane.coordinates.lat < LAT_MIN
+                  || airplane.coordinates.lat > LAT_MAX
+                  || airplane.coordinates.lon < LON_MIN
+                  || airplane.coordinates.lon > LON_MAX)
+                airplane.setAzimuth(airplane.getAzimuth() + 180);
+            });
 
-    private void foo() {
-        AirplaneKind kind = airplaneKinds.get(random.nextInt(airplaneKinds.size()));
-        Airplane airplane = new Airplane(kind, lastId++);
-        airplane.coordinates=new Coordinates(randomGenerators.generateRandomDoubleInRange(LAT_MIN, LAT_MAX),
-                randomGenerators.generateRandomDoubleInRangeWithNormalDistribution(LON_MIN, LON_MAX));
-        airplane.setAzimuth(randomGenerators.generateRandomDoubleInRange(0,360));
-        airplane.velocity = randomGenerators.generateRandomDoubleInRange(40, 70)*airplane.getAirplaneKind().getVelocityFactor();
-        airplanes.add(airplane);
-    }
-
-    private void UpdateSituation() {
-        try {
-            synchronized (lock) {
-                if (random.nextDouble() < CHANCE_FOR_NUMBER_CHANGE) { // chance to remove an airplane
-                    int indexToRemove;
-                    do {
-                        indexToRemove = random.nextInt(airplanes.size());
-                    } while (airplanes.get(indexToRemove).isAllocated());// don't remove allocated airplane
-                    airplanes.remove(indexToRemove);
-                }
-
-
-                airplanes.forEach(airplane -> {
-                    airplane.radialAcceleration = calculateNewRadialAcceleration(airplane);
-
-                    airplane.setAzimuth(airplane.getAzimuth() + airplane.radialAcceleration);
-                    airplane.coordinates.lat += Math.sin(worldAzimuthToEuclidRadians(airplane.getAzimuth())) * airplane.velocity / 100000;
-                    airplane.coordinates.lon += Math.cos(worldAzimuthToEuclidRadians(airplane.getAzimuth())) * airplane.velocity / 100000;
-                    if (airplane.coordinates.lat < LAT_MIN || airplane.coordinates.lat > LAT_MAX ||
-                            airplane.coordinates.lon < LON_MIN || airplane.coordinates.lon > LON_MAX)
-                        airplane.setAzimuth(airplane.getAzimuth() + 180);
-                });
-
-                if (random.nextDouble() < CHANCE_FOR_NUMBER_CHANGE) { // chance to add an airplane
-                    foo();
-                }
-            }
+        if (random.nextDouble() < CHANCE_FOR_NUMBER_CHANGE) { // chance to add an airplane
+          foo();
         }
-        catch (Exception e){
-            System.err.println("Error while updating air situation picture" + e.getMessage());
-            e.printStackTrace();
-        }
+      }
+    } catch (Exception e) {
+      System.err.println("Error while updating air situation picture" + e.getMessage());
+      e.printStackTrace();
     }
+  }
 
-    private double calculateNewRadialAcceleration(Airplane airplane) {
-        if (airplane.isAllocated()) {
-            Coordinates currLocation = airplane.coordinates;
-            Coordinates headingTo = airplane.headingTo;
+  private double calculateNewRadialAcceleration(Airplane airplane) {
+    if (airplane.isAllocated()) {
+      Coordinates currLocation = airplane.coordinates;
+      Coordinates headingTo = airplane.headingTo;
 
-            if (arrivedToDestination(currLocation, headingTo)) {
-                airplane.raiseArrivedAtDestinationEvent();
-            }
+      if (arrivedToDestination(currLocation, headingTo)) {
+        airplane.raiseArrivedAtDestinationEvent();
+      }
 
-            double azimuthToDestenation = geographicCalculations.azimuthBetween(currLocation, headingTo);
-            double differnceOfAzimuth = 180-geographicCalculations.normalizeAzimuth(azimuthToDestenation - airplane.getAzimuth());
+      double azimuthToDestenation = geographicCalculations.azimuthBetween(currLocation, headingTo);
+      double differnceOfAzimuth =
+          180
+              - geographicCalculations.normalizeAzimuth(
+                  azimuthToDestenation - airplane.getAzimuth());
 
-            return (differnceOfAzimuth > 0 ? Math.min(AZIMUTH_STEP*10, differnceOfAzimuth/5) : Math.max(-AZIMUTH_STEP*10, differnceOfAzimuth/5))/2;
+      return (differnceOfAzimuth > 0
+              ? Math.min(AZIMUTH_STEP * 10, differnceOfAzimuth / 5)
+              : Math.max(-AZIMUTH_STEP * 10, differnceOfAzimuth / 5))
+          / 2;
 
-        }
-        else {
-            if (random.nextDouble() < CHANCE_FOR_AZIMUTH_CHANGE)       // chance for any change
-                if (random.nextDouble() < CHANCE_FOR_AZIMUTH_CHANGE)   // chance for big change
-                    return randomGenerators.generateRandomDoubleInRange(-AZIMUTH_STEP, AZIMUTH_STEP);
-                else   // small gradual change, with a 66% chance that the size of the acceleration will be reduced
-                    return randomGenerators.generateRandomDoubleInRange(0, 1.5 * airplane.radialAcceleration);
-            return airplane.radialAcceleration;
-        }
+    } else {
+      if (random.nextDouble() < CHANCE_FOR_AZIMUTH_CHANGE) // chance for any change
+      if (random.nextDouble() < CHANCE_FOR_AZIMUTH_CHANGE) // chance for big change
+        return randomGenerators.generateRandomDoubleInRange(-AZIMUTH_STEP, AZIMUTH_STEP);
+        else // small gradual change, with a 66% chance that the size of the acceleration will be
+             // reduced
+        return randomGenerators.generateRandomDoubleInRange(0, 1.5 * airplane.radialAcceleration);
+      return airplane.radialAcceleration;
     }
+  }
 
-    private boolean arrivedToDestination(Coordinates currLocation, Coordinates headingTo) {
-        return geographicCalculations.distanceBetween(currLocation, headingTo) < 500;
-    }
+  private boolean arrivedToDestination(Coordinates currLocation, Coordinates headingTo) {
+    return geographicCalculations.distanceBetween(currLocation, headingTo) < 500;
+  }
 
-    /**
-     * Gets world azimuth - degrees in which 0 is up and increases clockwise and converts it to
-     * radians in which 0 is right and increases counter clockwise.
-     */
-    private double worldAzimuthToEuclidRadians(double azimuth) {
-        double inEuclidDegrees = -azimuth + 90;
-        return inEuclidDegrees * Math.PI / 180;
-    }
+  /**
+   * Gets world azimuth - degrees in which 0 is up and increases clockwise and converts it to
+   * radians in which 0 is right and increases counter clockwise.
+   */
+  private double worldAzimuthToEuclidRadians(double azimuth) {
+    double inEuclidDegrees = -azimuth + 90;
+    return inEuclidDegrees * Math.PI / 180;
+  }
 
-    @Override
-    public List<Airplane> getAllAirplanes() {
-        synchronized (lock){
-            return new ArrayList<>(airplanes);
-        }
+  @Override
+  public List<Airplane> getAllAirplanes() {
+    synchronized (lock) {
+      return new ArrayList<>(airplanes);
     }
+  }
 }
--- src/main/java/iaf/ofek/hadracha/base_course/web_server/EjectedPilotRescue/EjectionsImporter.java
+++ src/main/java/iaf/ofek/hadracha/base_course/web_server/EjectedPilotRescue/EjectionsImporter.java
@@ -26,63 +26,72 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class EjectionsImporter {
 
-    @Value("${ejections.server.url}")
-    public String EJECTION_SERVER_URL;
-    @Value("${ejections.namespace}")
-    public String NAMESPACE;
+  @Value("${ejections.server.url}")
+  public String EJECTION_SERVER_URL;
 
+  @Value("${ejections.namespace}")
+  public String NAMESPACE;
 
-    private AirplanesAllocationManager airplanesAllocationManager;
-    private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-    private final RestTemplate restTemplate;
-    private final CrudDataBase dataBase;
-    private final ListOperations listOperations;
-    private static final Double SHIFT_NORTH = 1.7;
+  private AirplanesAllocationManager airplanesAllocationManager;
+  private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+  private final RestTemplate restTemplate;
+  private final CrudDataBase dataBase;
+  private final ListOperations listOperations;
+  private static final Double SHIFT_NORTH = 1.7;
 
-    public EjectionsImporter(RestTemplateBuilder restTemplateBuilder, 
-                             CrudDataBase dataBase, 
-                             ListOperations listOperations, 
-                             @Autowired AirplanesAllocationManager airplanesAllocationManager) {
-        restTemplate = restTemplateBuilder.build();
-        this.dataBase = dataBase;
-        this.listOperations = listOperations;
-        this.airplanesAllocationManager = airplanesAllocationManager;
-        executor.scheduleAtFixedRate(this::updateEjections, 1, 1, TimeUnit.SECONDS);
+  public EjectionsImporter(
+      RestTemplateBuilder restTemplateBuilder,
+      CrudDataBase dataBase,
+      ListOperations listOperations,
+      @Autowired AirplanesAllocationManager airplanesAllocationManager) {
+    restTemplate = restTemplateBuilder.build();
+    this.dataBase = dataBase;
+    this.listOperations = listOperations;
+    this.airplanesAllocationManager = airplanesAllocationManager;
+    executor.scheduleAtFixedRate(this::updateEjections, 1, 1, TimeUnit.SECONDS);
+  }
 
-    }
-
-    private void updateEjections() {
-        try {
-            List<EjectedPilotInfo> ejectionsFromServer;
-            ResponseEntity<List<EjectedPilotInfo>> responseEntity = restTemplate.exchange(
-                    EJECTION_SERVER_URL + "/ejections?name=" + NAMESPACE, HttpMethod.GET,
-                    null, new ParameterizedTypeReference<List<EjectedPilotInfo>>() {
-                    });
-            ejectionsFromServer = responseEntity.getBody();
-            if (ejectionsFromServer != null) {
-                for(EjectedPilotInfo ejectedPilotInfo: ejectionsFromServer) {
-                    ejectedPilotInfo.coordinates.lat += SHIFT_NORTH;
-                }
-            }
-            List<EjectedPilotInfo> updatedEjections = ejectionsFromServer;
-            List<EjectedPilotInfo> previousEjections = dataBase.getAllOfType(EjectedPilotInfo.class);
-
-            List<EjectedPilotInfo> addedEjections = ejectionsToAdd(updatedEjections, previousEjections);
-            List<EjectedPilotInfo> removedEjections = ejectionsToRemove(updatedEjections, previousEjections);
-
-            addedEjections.forEach(dataBase::create);
-            removedEjections.stream().map(EjectedPilotInfo::getId).forEach(id -> dataBase.delete(id, EjectedPilotInfo.class));
-        } catch (RestClientException e) {
-            System.err.println("Could not get ejections: " + e.getMessage());
-            e.printStackTrace();
+  private void updateEjections() {
+    try {
+      List<EjectedPilotInfo> ejectionsFromServer;
+      ResponseEntity<List<EjectedPilotInfo>> responseEntity =
+          restTemplate.exchange(
+              EJECTION_SERVER_URL + "/ejections?name=" + NAMESPACE,
+              HttpMethod.GET,
+              null,
+              new ParameterizedTypeReference<List<EjectedPilotInfo>>() {});
+      ejectionsFromServer = responseEntity.getBody();
+      if (ejectionsFromServer != null) {
+        for (EjectedPilotInfo ejectedPilotInfo : ejectionsFromServer) {
+          ejectedPilotInfo.getCoordinates().lat += SHIFT_NORTH;
         }
-    }
+      }
+      List<EjectedPilotInfo> updatedEjections = ejectionsFromServer;
+      List<EjectedPilotInfo> previousEjections = dataBase.getAllOfType(EjectedPilotInfo.class);
 
-    private List<EjectedPilotInfo> ejectionsToRemove(List<EjectedPilotInfo> updatedEjections, List<EjectedPilotInfo> previousEjections) {
-        return listOperations.subtract(previousEjections, updatedEjections, new Entity.ByIdEqualizer<>());
-    }
+      List<EjectedPilotInfo> addedEjections = ejectionsToAdd(updatedEjections, previousEjections);
+      List<EjectedPilotInfo> removedEjections =
+          ejectionsToRemove(updatedEjections, previousEjections);
 
-    private List<EjectedPilotInfo> ejectionsToAdd(List<EjectedPilotInfo> updatedEjections, List<EjectedPilotInfo> previousEjections) {
-        return listOperations.subtract(updatedEjections, previousEjections, new Entity.ByIdEqualizer<>());
+      addedEjections.forEach(dataBase::create);
+      removedEjections.stream()
+          .map(EjectedPilotInfo::getId)
+          .forEach(id -> dataBase.delete(id, EjectedPilotInfo.class));
+    } catch (RestClientException e) {
+      System.err.println("Could not get ejections: " + e.getMessage());
+      e.printStackTrace();
     }
+  }
+
+  private List<EjectedPilotInfo> ejectionsToRemove(
+      List<EjectedPilotInfo> updatedEjections, List<EjectedPilotInfo> previousEjections) {
+    return listOperations.subtract(
+        previousEjections, updatedEjections, new Entity.ByIdEqualizer<>());
+  }
+
+  private List<EjectedPilotInfo> ejectionsToAdd(
+      List<EjectedPilotInfo> updatedEjections, List<EjectedPilotInfo> previousEjections) {
+    return listOperations.subtract(
+        updatedEjections, previousEjections, new Entity.ByIdEqualizer<>());
+  }
 }
